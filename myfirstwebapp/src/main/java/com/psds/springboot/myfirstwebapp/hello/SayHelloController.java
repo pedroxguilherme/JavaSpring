ede2a902707b715ae629acdf3be14b4a06446971
Pedro Guilherme <89921744+pedroxguilherme@users.noreply.github.com>
2023-08-22 16:47:59 -0300

    firstwebapplication

---
+++ myfirstwebapp/src/main/java/com/psds/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,44 @@
+package com.psds.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+	
+	
+	
+
+@RequestMapping("say-hello")
+@ResponseBody
+public String Sayhello() {
+	return "Hello World without html";
+	
+}	
+@RequestMapping("say-hello-html")
+@ResponseBody	
+public String HelloHtml() {
+	
+	StringBuffer sb = new StringBuffer();
+	sb.append("<html>");
+	sb.append("<head>");
+	sb.append("<title> My first Html web</title>");
+	sb.append("<head>");
+	sb.append("<body>");
+	sb.append("<p>My first Html web</p>");
+	sb.append("</body>");
+	sb.append("</html>");
+	
+	
+	return sb.toString();
+}
+
+
+	
+	
+	
+	
+
+}
