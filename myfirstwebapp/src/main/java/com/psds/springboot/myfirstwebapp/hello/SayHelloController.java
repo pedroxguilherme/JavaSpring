c52f73a882b9ddccec0fcbfc4afd48b6f7c69c2d
Pedro Guilherme <89921744+pedroxguilherme@users.noreply.github.com>
2023-08-25 13:38:24 -0300

    Requestparam

--- myfirstwebapp/src/main/java/com/psds/springboot/myfirstwebapp/hello/SayHelloController.java
+++ myfirstwebapp/src/main/java/com/psds/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -1,15 +1,17 @@
 package com.psds.springboot.myfirstwebapp.hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SayHelloController {
 	
-	
-	
+private Logger logger = LoggerFactory.getLogger(getClass());
 
 @RequestMapping("say-hello")
 @ResponseBody
@@ -41,7 +43,15 @@ public String HelloJsp() {
 	
 	return "sayHello";
 }	
-	
+
+@RequestMapping("login")
+public String GotologinJsp(@RequestParam String name, ModelMap model) {
+	model.put("name", name);
+	logger.debug("Name:", name);
+	logger.info("Printing in info level");
+	return "login";
+}	
+		
 	
 	
 
